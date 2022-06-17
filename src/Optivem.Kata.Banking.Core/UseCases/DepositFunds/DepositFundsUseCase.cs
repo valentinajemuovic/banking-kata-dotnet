b57d7ebcec4861c7c3fd33437116a7327ab0f897
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-17 18:27:43 +0200

    #16 DepositFundsUseCase - MainFlow - Skeleton

---
+++ src/Optivem.Kata.Banking.Core/UseCases/DepositFunds/DepositFundsUseCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.Kata.Banking.Core.UseCases.DepositFunds
+{
+    public class DepositFundsUseCase : IVoidUseCase<DepositFundsRequest>
+    {
+        public Task HandleAsync(DepositFundsRequest request)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ test/Optivem.Kata.Banking.Test/UseCases/DepositFundsUseCaseTest.cs
@@ -0,0 +1,30 @@
+﻿using Optivem.Kata.Banking.Core.UseCases.DepositFunds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Optivem.Kata.Banking.Test.UseCases
+{
+    public class DepositFundsUseCaseTest
+    {
+        private readonly DepositFundsUseCase _useCase; 
+
+        public DepositFundsUseCaseTest()
+        {
+            _useCase = new DepositFundsUseCase();
+        }
+
+
+        [Fact]
+        public async void Nothing()
+        {
+            var request = new DepositFundsRequest();
+
+            await _useCase.HandleAsync(request);
+        }
+
+    }
+}
