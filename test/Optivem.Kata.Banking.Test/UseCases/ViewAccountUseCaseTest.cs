d72b364c3cc4f0095298b51165c864af12cff149
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-17 18:01:35 +0200

    #15 ViewAccountUseCase - ExceptionalFlow - Empty account number

--- test/Optivem.Kata.Banking.Test/UseCases/ViewAccountUseCaseTest.cs
+++ test/Optivem.Kata.Banking.Test/UseCases/ViewAccountUseCaseTest.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
 using Optivem.Kata.Banking.Core.Domain.BankAccounts;
+using Optivem.Kata.Banking.Core.Exceptions;
 using Optivem.Kata.Banking.Core.UseCases.ViewAccount;
 using Optivem.Kata.Banking.Infrastructure.Fake.BankAccounts;
 using Optivem.Kata.Banking.Test.Common.Builders.RequestBuilders;
+using Optivem.Kata.Banking.Test.Common.Data;
 using Optivem.Kata.Banking.Test.Common.Setup;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+
+using static Optivem.Kata.Banking.Test.Common.Builders.RequestBuilders.ViewAccountRequestBuilder;
 
 namespace Optivem.Kata.Banking.Test.UseCases
 {
@@ -35,7 +39,7 @@ namespace Optivem.Kata.Banking.Test.UseCases
 
             _bankAccountRepository.AlreadyContains(accountNumber, firstName, lastName, balance);
 
-            var request = ViewAccountRequestBuilder.ViewAccount()
+            var request = ViewAccount()
                 .AccountNumber(accountNumber)
                 .Build();
 
@@ -49,6 +53,18 @@ namespace Optivem.Kata.Banking.Test.UseCases
             var response = await _useCase.HandleAsync(request);
 
             response.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [Theory]
+        [ClassData(typeof(NullEmptyWhitespaceStringData))]
+        public async Task Should_throw_exception_given_empty_account_number(string accountNumber)
+        {
+            var request = ViewAccount().AccountNumber(accountNumber).Build();
+
+            Func<Task> action = () => _useCase.HandleAsync(request);
+
+            await action.Should().ThrowAsync<ValidationException>()
+                .WithMessage(ValidationMessages.AccountNumberEmpty);
         }
     }
 }
