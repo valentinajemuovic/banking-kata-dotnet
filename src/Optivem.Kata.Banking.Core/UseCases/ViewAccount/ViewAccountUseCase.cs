bba20702dcb60786709c3c265feb128b89634681
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-17 17:03:32 +0200

    #12 ViewAccountUseCase - MainFlow - Skeleton

---
+++ src/Optivem.Kata.Banking.Core/UseCases/ViewAccount/ViewAccountUseCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.Kata.Banking.Core.UseCases.ViewAccount
+{
+    public class ViewAccountUseCase : IUseCase<ViewAccountRequest, ViewAccountResponse>
+    {
+        public Task<ViewAccountResponse> HandleAsync(ViewAccountRequest request)
+        {
+            var response = new ViewAccountResponse();
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ test/Optivem.Kata.Banking.Test/UseCases/ViewAccountUseCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Optivem.Kata.Banking.Core.UseCases.ViewAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Optivem.Kata.Banking.Test.UseCases
+{
+    public class ViewAccountUseCaseTest
+    {
+        private readonly ViewAccountUseCase _useCase;
+
+        public ViewAccountUseCaseTest()
+        {
+            _useCase = new ViewAccountUseCase();
+        }
+
+        [Fact]
+        public async Task Nothing()
+        {
+            var request = new ViewAccountRequest();
+
+            var response = await _useCase.HandleAsync(request);
+
+            response.Should().NotBeNull();
+        }
+    }
+}
