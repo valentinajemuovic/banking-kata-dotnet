c754a8a9704598dcc2a34645c9fd4fcacfb1f1f8
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-18 16:09:36 +0200

    #24 Infrastructure - DateTimeService - Ulid

---
+++ src/Optivem.Kata.Banking.Infrastructure/AccountNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using NUlid;
+using Optivem.Kata.Banking.Core.Domain.BankAccounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.Kata.Banking.Infrastructure
+{
+    public class AccountNumberGenerator : IAccountNumberGenerator
+    {
+        public AccountNumber Next()
+        {
+            var value = Ulid.NewUlid().ToString();
+            return AccountNumber.From(value);
+        }
+    }
+}
---
+++ test/Optivem.Kata.Banking.Test/Infrastructure/AccountNumberGeneratorTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Optivem.Kata.Banking.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Optivem.Kata.Banking.Test.Infrastructure
+{
+    public class AccountNumberGeneratorTest
+    {
+        private readonly AccountNumberGenerator _generator; 
+
+        public AccountNumberGeneratorTest()
+        {
+            _generator = new AccountNumberGenerator();
+        }
+
+        [Fact]
+        public void Should_generate_account_number()
+        {
+            var accountNumber = _generator.Next();
+            accountNumber.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Should_generate_unique_account_numbers()
+        {
+            var accountNumber1 = _generator.Next();
+            var accountNumber2 = _generator.Next();
+            var accountNumber3 = _generator.Next();
+
+            accountNumber2.Should().NotBe(accountNumber1);
+            accountNumber3.Should().NotBe(accountNumber2);
+        }
+    }
+}
