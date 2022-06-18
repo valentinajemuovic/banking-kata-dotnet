fae064986390755dd946fe6e6182209584c8d1eb
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-18 16:17:55 +0200

    #25 Infrastructure - BankAccountRepository - EF

---
+++ src/Optivem.Kata.Banking.Infrastructure/BankAccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using Optivem.Kata.Banking.Core.Domain.BankAccounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optivem.Kata.Banking.Infrastructure
+{
+    public class BankAccountRepository : IBankAccountRepository
+    {
+        public void Add(BankAccount bankAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BankAccount?> GetByAccountNumberAsync(AccountNumber accountNumber)
+        {
+            var bankAccount = (BankAccount?) null;
+            return Task.FromResult(bankAccount);
+        }
+
+        public void Update(BankAccount bankAccount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Optivem.Kata.Banking.Test/Infrastructure/BankAccountRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Optivem.Kata.Banking.Core.Domain.BankAccounts;
+using Optivem.Kata.Banking.Infrastructure;
+using Optivem.Kata.Banking.Test.Common.Builders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Optivem.Kata.Banking.Test.Infrastructure
+{
+    public class BankAccountRepositoryTest
+    {
+        private readonly BankAccountRepository _repository;
+
+        public BankAccountRepositoryTest()
+        {
+            _repository = new BankAccountRepository();
+        }
+
+        [Fact]
+        public async Task Should_return_null_given_non_existent_account_number()
+        {
+            var accountNumber = AccountNumber.From(BankAccountDefaults.DefaultAccountNumber);
+
+            // TODO: VC: Refactor name
+            var bankAccount = await _repository.GetByAccountNumberAsync(accountNumber);
+
+            bankAccount.Should().BeNull();
+        } 
+    }
+}
