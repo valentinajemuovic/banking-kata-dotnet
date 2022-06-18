65716df1014e46cc9d3427694c3f2a5f4ab1e0f5
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-18 15:56:51 +0200

    #23 Infrastructure - DateTimeService - System clock

---
+++ src/Optivem.Kata.Banking.Infrastructure/DateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using Optivem.Kata.Banking.Core.Domain.Time;
+
+namespace Optivem.Kata.Banking.Infrastructure
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ test/Optivem.Kata.Banking.Test/Infrastructure/DateTimeServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Optivem.Kata.Banking.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Optivem.Kata.Banking.Test.Infrastructure
+{
+    public class DateTimeServiceTest
+    {
+        private readonly DateTimeService _dateTimeService;
+
+        public DateTimeServiceTest()
+        {
+            _dateTimeService = new DateTimeService();
+        }
+
+        [Fact]
+        public void Should_return_current_date_time()
+        {
+            var now = _dateTimeService.Now();
+            now.Should().BeAfter(DateTime.MinValue);
+        }
+    }
+}
